4e897005fcbf73b927879ed703536d85da3440c7
Kyle Carhart <kyle.carhart@capgemini.com>
2022-09-22 14:10:00 -0400

    added extended false

--- express-session-jwt/src/server.ts
+++ express-session-jwt/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(compression());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send(`Hello!`);
